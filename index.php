c391078bf72edfda44afee3a7691da97ce1112a1
Teoldra <mare_14@web.de>
2024-01-09 16:40:15 +0100

    Php function die Bilder aus Slider Ordner zieht

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+
+$verzeichnis = "img/slider/";
+// Array initialisieren
+$bildArray = array();
+// Verzeichnis öffnen
+$verzeichnisinhalt = scandir($verzeichnis);
+
+// Durch Verzeichnisinhalt iterieren
+foreach ($verzeichnisinhalt as $datei) {
+	// Nur Dateien berücksichtigen (keine Verzeichnisse)
+	if (is_file($verzeichnis . $datei) && pathinfo($verzeichnis . $datei, PATHINFO_EXTENSION) === 'jpg') {
+		// Pfad und Dateiname zum Array hinzufügen
+		$bildArray[] = $verzeichnis . $datei;
+	}
+}
+$bildlength = count($bildArray);
+
+foreach($verzeichnisinhalt as $datei){
+
+    $pattern = '/\..*/';
+    $replace = "";
+    $filename = preg_replace($pattern, $replace, $datei);
+	$pattern2 = '/-[0-9]/';
+	$replace2 = "";
+    $filename = preg_replace($pattern2, $replace2, $filename);
+
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NieR Automata</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<link rel="icon" href="img/dog-fav-icon.png">
+	<link rel="stylesheet" type="text/css" href="css/index-css.css">
+
+</head>
+
+<body>
+
+	<!-- Header element -->
+	<header>
+		<div class="headerSlider">
+			<?php
+			for ($i = 0; $i < $bildlength; $i++) {
+				echo
+					'<div class="slides fade">
+            <img src="' . $bildArray[$i] . '">
+          </div>';
+			}
+			?>
+		</div>
+		<a class="prev">&#10094;</a>
+		<a class="next">&#10095;</a>
+	</header>
+	<!-- The dots/circles -->
+	<div style="text-align:center">
+		<?php
+		for ($i = 0; $i < $bildlength; $i++) {
+			echo '<span class="dot"></span>';
+		}
+		?>
+	</div>
+
+	<!-- Main element -->
+	<main>
+		<h2>About NieR Automata</h2>
+
+		<p>Im Jahr 11.945 setzt die Geschichte von Nier: Automata an. Fremdartige Wesen überfielen mit einer Armee aus
+			Maschinen die Erde und vernichten seitdem sämtliches humanoides Leben. Der überlebende Teil der Menschheit
+			zieht sich auf den Mond zurück und organisiert von dort den Widerstand.</p>
+		<div class="mainImg">
+			<img src="img/about-nier-automata.jpg">
+			<p class="img-txt">Artwork</p>
+		</div>
+
+		<!-- Accordion -->
+		<!-- Inhalt 1 -->
+		<button class="accordion">2B</button>
+		<div class="panel">
+			<div>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nostrum voluptatum debitis ullam
+					commodi alias id magnam non hic cum. Id ducimus, optio rem deleniti ea ipsa soluta ipsum non!</p>
+			</div>
+			<div class="accImg">
+				<img src="img/2b-dust-art.jpg" alt="2b-dust-art">
+			</div>
+		</div>
+
+		<!-- Inhalt 2 -->
+		<button class="accordion">9S</button>
+		<div class="panel">
+			<div>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nostrum voluptatum debitis ullam
+					commodi alias id magnam non hic cum. Id ducimus, optio rem deleniti ea ipsa soluta ipsum non!</p>
+			</div>
+			<div class="accImg">
+				<img src="img/9s-art.jpg" alt="2b-dust-art">
+			</div>
+		</div>
+
+		<!-- Inhalt 2 -->
+		<button class="accordion">A2</button>
+		<div class="panel">
+			<div>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nostrum voluptatum debitis ullam
+					commodi alias id magnam non hic cum. Id ducimus, optio rem deleniti ea ipsa soluta ipsum non!</p>
+			</div>
+			<div class="accImg">
+				<img src="img/a2-art.jpg" alt="2b-dust-art">
+			</div>
+		</div>
+
+
+
+		<!-- 2B Characteristics -->
+		<h2>2B Characteristics</h2>
+		<ol>
+			<li><b>Determination</b>: 2B is extremely determined and focused on her mission. She goes to great lengths
+				to fulfill her tasks and demonstrates a willingness to make sacrifices for the betterment of humanity.
+			</li>
+			<li><b>Composure</b>: 2B often displays a calm and composed demeanor, even in dangerous or stressful
+				situations. This cool composure helps her make wise decisions and effectively deal with challenges in
+				her environment.</li>
+			<li><b>Aura</b>: 2B carries a mysterious aura around her. Her character conceals deeper emotions and
+				backgrounds that are gradually unveiled as the story progresses. This mysterious aspect makes her
+				intriguing for players and adds depth to her personality.</li>
+		</ol>
+
+		<!-- Dog breeds -->
+		<h2>Side Characters</h2>
+		<p>Here are some Side Charakters:</p>
+		<ul>
+			<li>Commander</li>
+			<li>Operator 6O</li>
+			<li>Operator 21O</li>
+			<li>Pascal</li>
+			<li>Emil</li>
+			<li>Adam</li>
+			<li>Eve</li>
+		</ul>
+	</main>
+
+	<!-- Footer element -->
+	<footer>
+		<p>This Site was created by: &copy; Marcel</p>
+
+
+</body>
+<script language="javascript" type="text/javascript" src="javascript/index-js.js"></script>
+
+</html>
